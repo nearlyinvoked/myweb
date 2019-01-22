a78ac398373516c3c554e457f7da03b44f8b4c71
Ilham Habibie <ilhamha@oloopstudio.com>
2019-01-22 20:49:45 +0700

    uncomment page news

--- wp-content/themes/mytheme/page-news.php
+++ wp-content/themes/mytheme/page-news.php
@@ -4,22 +4,22 @@
     <div class="container">
         <h1>News</h1>
         <?php
-        //$newsPosts = new WP_Query('news');
+        $newsPosts = new WP_Query('news');
 
-        //if ($newsPosts->have_posts()) :
-            //while ($newsPosts->have_posts()) : $newsPosts->the_post(); ?>
+        if ($newsPosts->have_posts()) :
+            while ($newsPosts->have_posts()) : $newsPosts->the_post(); ?>
                 <h3>
-                    <a href="<?php //the_permalink(); ?>"><?php //the_title(); ?>
+                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?>
                 </a>
                 </h3>
-                <?php //the_excerpt(); ?>
+                <?php the_excerpt(); ?>
         <?php
-        //     endwhile;
+             endwhile;
 
-        //     else :
-        //         // No News
-        // endif;
-        //wp_reset_postdata();
+            else :
+                // No News
+        endif;
+        wp_reset_postdata();
         ?>
     </div>
     <?php
